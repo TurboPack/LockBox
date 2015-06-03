ff090c560505983cb63cbe325d206afa0e6a294b
Roman Kassebaum <roman.kassebaum@kdv-dt.de>
2015-06-03 18:22:15 +0200

    Compiled with C++Builder.

--- packages/CBuilder/LockBoxCD.cpp
+++ packages/CBuilder/LockBoxCD.cpp
@@ -2,9 +2,6 @@
 
 #include <basepch.h>
 #pragma hdrstop
-USEFORMNS("..\..\source\LbDesign.pas", Lbdesign, LbAboutForm);
-USEFORMNS("..\..\source\LbKeyEd1.pas", Lbkeyed1, frmSymmetricKey);
-USEFORMNS("..\..\source\LbKeyEd2.pas", Lbkeyed2, frmRSAKeys);
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 //---------------------------------------------------------------------------
